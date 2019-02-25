702bb77f4a58ab0356f921ad2fe98cc73ce428e9
ananthrajsingh <ananthraj.singh@gmail.com>
2019-02-25 11:00:27 +0530

    Add .cpp and .h files

--- NDKExample/app/src/main/cpp/SuperpoweredExample.cpp
+++ NDKExample/app/src/main/cpp/SuperpoweredExample.cpp
@@ -1,52 +1,114 @@
-#ifndef Header_SuperpoweredExample
-#define Header_SuperpoweredExample
-
-#include <math.h>
-#include <pthread.h>
-
 #include "SuperpoweredExample.h"
 #include <SuperpoweredAdvancedAudioPlayer.h>
-#include <SuperpoweredFilter.h>
-#include <SuperpoweredRoll.h>
-#include <SuperpoweredFlanger.h>
-#include <AndroidIO/SuperpoweredAndroidAudioIO.h>
-#include <SuperpoweredTimeStretching.h>
+#include <SuperpoweredSimple.h>
+#include <jni.h>
+#include <stdio.h>
+#include <android/log.h>
+#include <stdlib.h>
+#include <SLES/OpenSLES.h>
+#include <SLES/OpenSLES_AndroidConfiguration.h>
 
-#define HEADROOM_DECIBEL 3.0f
-static const float headroom = powf(10.0f, -HEADROOM_DECIBEL * 0.025f);
 
-enum TEMPO_MODE {
-    SLOW_DOWN, PLAY_NORMAL, SPEED_UP
-};
+static bool audioProcessing(void *clientdata, short int *audioIO, int numberOfSamples,
+                            int __unused samplerate) {
+    return ((SuperpoweredExample *) clientdata)->process(audioIO, (unsigned int) numberOfSamples);
+}
 
-static const float TEMPO_ORIGINAL = 1.0f;
-static const float TEMPO_HALF = TEMPO_ORIGINAL / 2;
-static const float TEMPO_DOUBLE = TEMPO_ORIGINAL * 2;
+SuperpoweredExample::SuperpoweredExample(unsigned int samplerate, unsigned int buffersize,
+                                         const char *path, int fileOffset, int fileLength) : volume(1.0f * headroom) {
+    stereoBuffer = (float *) memalign(16, (buffersize + 16) * sizeof(float) * 2);
 
-class SuperpoweredExample {
-public:
+    player = new SuperpoweredAdvancedAudioPlayer(&player, NULL, samplerate, 0);
+    player->open(path, fileOffset, fileLength);
+//    SuperpoweredAndroidAudioIO(int samplerate, int buffersize, bool enableInput, bool enableOutput
+// , audioProcessingCallback callback, void *clientdata, int inputStreamType = -1, int outputStreamType = -1);
 
-    SuperpoweredExample(unsigned int samplerate, unsigned int buffersize, const char *path,
-                        int fileOffset, int fileLength);
+    audioSystem = new SuperpoweredAndroidAudioIO(samplerate, buffersize, false, true,
+                                                 audioProcessing,
+                                                 this, -1, SL_ANDROID_STREAM_MEDIA);
+}
 
-    ~SuperpoweredExample();
+SuperpoweredExample::~SuperpoweredExample() {
+    delete audioSystem;
+    delete player;
+    free(stereoBuffer);
+}
 
-    bool process(short int *output, unsigned int numberOfSamples);
+void SuperpoweredExample::onPlayPause(bool play) {
+    if (play) {
+        player->play(false);
+    } else {
+        player->pause();
+    }
+}
 
-    void onPlayPause(bool play);
+void SuperpoweredExample::onTempoSelected(int tempoMode) {
+    __android_log_print(ANDROID_LOG_VERBOSE, "SuperpoweredExample", "TIME_SELECTED: %i", tempoMode);
 
-    void seekTo(double positionPercent);
+    switch (tempoMode) {
+        case SLOW_DOWN:
+            player->setTempo(TEMPO_HALF, true);
+            break;
+        case PLAY_NORMAL:
+            player->setTempo(TEMPO_ORIGINAL, true);
+            break;
+        case SPEED_UP:
+            player->setTempo(TEMPO_DOUBLE, true);
+            break;
+        default:
+            player->setTempo(TEMPO_ORIGINAL, true);
+            break;
+    }
+}
 
-    void onTempoSelected(int tempoMode);
+void SuperpoweredExample::onPitchChanged(int pitchValue) {
+    __android_log_print(ANDROID_LOG_VERBOSE, "SuperpoweredExample", "PITCH_CHANGED: %i",
+                        pitchValue);
+    player->setPitchShift(pitchValue);
+}
 
-    void onPitchChanged(int pitchValue);
 
-private:
-    SuperpoweredAndroidAudioIO *audioSystem;
-    SuperpoweredAdvancedAudioPlayer *player;
+void SuperpoweredExample::seekTo(double positionPercent) {
+    player->seek(positionPercent);
+}
 
-    float *stereoBuffer;
-    float volume;
-};
+bool SuperpoweredExample::process(short int *output, unsigned int numberOfSamples) {
+    bool shouldPlay = player->process(stereoBuffer, false, numberOfSamples, volume);
+    if (shouldPlay) {
+        SuperpoweredFloatToShortInt(stereoBuffer, output, numberOfSamples);
+    }
+    return shouldPlay;
+}
 
-#endif
+static SuperpoweredExample *example = NULL;
+
+extern "C" {
+JNIEXPORT void Java_com_example_superpoweredsample_MainActivity_SuperpoweredExample(JNIEnv *javaEnvironment,
+                                                                                    jobject __unused obj,
+                                                                                    jint samplerate,
+                                                                                    jint buffersize,
+                                                                                    jstring apkPath,
+                                                                                    jint fileOffset,
+                                                                                    jint fileLength) {
+
+    const char *path = javaEnvironment->GetStringUTFChars(apkPath, JNI_FALSE);
+    example = new SuperpoweredExample((unsigned int) samplerate, (unsigned int) buffersize, path,
+                                      fileOffset, fileLength);
+    javaEnvironment->ReleaseStringUTFChars(apkPath, path);
+}
+
+JNIEXPORT void Java_com_example_superpoweredsample_MainActivity_onPlayPause(JNIEnv *__unused javaEnvironment, jobject __unused obj, jboolean play) {
+    example->onPlayPause(play);
+}
+
+JNIEXPORT void Java_com_example_superpoweredsample_MainActivity_onPitchChanged(JNIEnv *__unused javaEnvironment, jobject __unused obj, jint pitchValue) {
+    example->onPitchChanged(pitchValue);
+}
+JNIEXPORT void Java_com_example_superpoweredsample_MainActivity_onTempoSelected(JNIEnv *__unused javaEnvironment, jobject __unused obj, jint timeStretchValue) {
+    example->onTempoSelected(timeStretchValue);
+}
+
+JNIEXPORT void Java_com_example_superpoweredsample_MainActivity_seekTo(JNIEnv *__unused javaEnvironment, jobject __unused obj, jdouble position) {
+    example->seekTo(position);
+}
+}
